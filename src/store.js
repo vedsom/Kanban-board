7962909ac7d6abd5218864863926d1a37141ba2a
Abhishek Pandey <abhipandey0314@gmail.com>
2023-11-02 00:29:48 +0530

    add ui component

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from '@reduxjs/toolkit';
+import {DataReducer, SelectDataReducer} from './Reducers/DataReducer';
+
+const store=configureStore({
+    reducer:{
+        DataReducer,SelectDataReducer
+    }
+})
+export default store;
